3351827d563347f6f35d63c015a2bb6f8903a36f
Arif <arifalihan2016@gmail.com>
2025-02-19 12:12:40 +0300

    "Şifreleme Yapmak İstermisiniz ?" butonu yerleştirildi  "Şifre Çözmek İstermisiniz ?" butonu yerleştirildi

--- SezarOdev/src/main/java/org/example/AnaSayfa.java
+++ SezarOdev/src/main/java/org/example/AnaSayfa.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileReader;
+
 public class AnaSayfa {
+    JFrame frame;
+    public AnaSayfa(){
+        frame = new JFrame("Şifreleme Uygulaması");
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(2,1));
+
+        JButton sifrelemeButonu = new JButton("Şifreleme Yapmak İstermisiniz?");
+        JButton sifreCozmeButonu = new JButton("Şifre Çözmek İstermisiniz ?");
+
+        frame.add(sifrelemeButonu);
+        frame.add(sifreCozmeButonu);
+
+        frame.setVisible(true);
+
+    }
 }
